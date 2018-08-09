a77f876f621386333c43cc8d39c64d7d45b72aab
Itamar <Itamar@PC-ESTUDO>
2018-08-09 15:15:13 -0300

    Exemplo12 - Fazer um programa para ler os valores da largura e altura de um retângulo

---
+++ Exemplo01/Exemplo12/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+/*
+ Fazer um programa para ler os valores da largura e altura
+de um retângulo. Em seguida, mostrar na tela o valor de
+sua área, perímetro e diagonal. Usar uma classe como
+mostrado no projeto ao lado.
+Entrada: Saída:
+3.00
+4.00
+AREA = 12.00
+PERIMETRO = 14.00
+DIAGONAL = 5.00
+     */
+
+namespace Exemplo12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo x = new Retangulo();
+
+            x.largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Area = " + x.area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Perimetro = " + x.perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Diagonal = " + x.diagonal().ToString("F2", CultureInfo.InvariantCulture));
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exemplo01/Exemplo12/Retangulo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo12
+{
+    class Retangulo
+    {
+        public double largura;
+        public double altura;
+
+
+        public double area()
+        {
+            return largura * altura;
+        }
+
+        public double perimetro()
+        {
+            return 2.0 * (largura + altura);
+        }
+
+        public double diagonal()
+        {
+            return Math.Sqrt(largura * largura + altura * altura);
+        }
+
+
+    }
+}
