90375ab46c04d9b36f76e4a09ebc3c64a2070f4e
Itamar <Itamar@PC-ESTUDO>
2018-08-09 14:53:40 -0300

    Exemplo11 - Fazer um programa para ler os dados de um produto em estoque

---
+++ Exemplo01/Exemplo11/Produto.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+/*
+ Fazer um programa para ler os dados de um produto em estoque (nome, preço e
+quantidade no estoque). Em seguida:
+• Mostrar os dados do produto (nome, preço, quantidade no estoque, valor total no
+estoque)
+• Realizar uma entrada no estoque e mostrar novamente os dados do produto
+• Realizar uma saída no estoque e mostrar novamente os dados do produto
+Para resolver este problema, você deve criar
+uma CLASSE conforme projeto ao lado:
+     */
+
+namespace Exemplo11
+{
+    class Produto
+    {
+        public string nome;
+        public double preco;
+        public int quantidadeEmEstoque;
+
+
+        public double valorTotalEmEstoque()
+        {
+            return preco * quantidadeEmEstoque;
+        }
+
+        public void realizarEntrada(int quantidade)
+        {
+            quantidadeEmEstoque = quantidadeEmEstoque + quantidade;
+        }
+
+        public void realizarSaida(int quantidade)
+        {
+            quantidadeEmEstoque = quantidadeEmEstoque - quantidade;
+        }
+
+
+        public override string ToString()
+        {
+            return nome
+                + ", R$:  "
+                + preco.ToString("F2", CultureInfo.InstalledUICulture)
+                + ", "
+                + quantidadeEmEstoque
+                + " unidades , Total R$: "
+                + valorTotalEmEstoque().ToString("F2", CultureInfo.InstalledUICulture);
+        }
+
+
+    }
+}
---
+++ Exemplo01/Exemplo11/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+/*
+ Fazer um programa para ler os dados de um produto em estoque (nome, preço e
+quantidade no estoque). Em seguida:
+• Mostrar os dados do produto (nome, preço, quantidade no estoque, valor total no
+estoque)
+• Realizar uma entrada no estoque e mostrar novamente os dados do produto
+• Realizar uma saída no estoque e mostrar novamente os dados do produto
+Para resolver este problema, você deve criar
+uma CLASSE conforme projeto ao lado:
+
+    Digite os dados do produto:
+Nome: TV
+Preço: 900.00
+Quantidade em estoque: 10
+Dados do produto: TV, R$ 900.00, 10 unidades, Total: R$ 9000.00
+Digite a quantidade de produtos que entraram no estoque: 5
+Dados atualizados: TV, R$ 900.00, 15 unidades, Total: R$ 13500.00
+Digite a quantidade de produtos que saíram do estoque: 3
+Dados atualizados: TV, R$ 900.00, 12 unidades, Total: R$ 10800.00
+
+     */
+
+
+namespace Exemplo11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+
+            Console.Write("Qual o nome do produto:");
+
+            p.nome = Console.ReadLine();
+
+            Console.Write("Qual o preço do produto:");
+            p.preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            Console.Write("Qual a quantidade de entrada no estoque :");
+            p.realizarEntrada(int.Parse(Console.ReadLine()));
+
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite a quantidade de produtos que entraram no estoque:");
+            p.realizarEntrada(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite a quantidade de produtos que sairam do estoque:");
+            p.realizarEntrada(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.ReadLine();
+
+        }
+    }
+}
