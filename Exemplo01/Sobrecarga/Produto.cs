eb04cce3d2c25cf24c0827e91180e04b6b32e53e
Itamar <Itamar@PC-ESTUDO>
2018-08-09 22:44:43 -0300

    SOBRECARGA: é o recurso que permite definir mais de uma versão da mesma operação, diferenciando-as pela lista de parâmetros

---
+++ Exemplo01/Sobrecarga/Produto.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Sobrecarga
+{
+    class Produto
+    {
+        public string nome;
+        public double preco;
+        public int quantidadeEmEstoque;
+
+
+        public Produto(string nome, double preco, int quantidadeEmEstoque)
+        {
+            this.nome = nome;
+            this.preco = preco;
+            this.quantidadeEmEstoque = quantidadeEmEstoque;
+        }
+
+        //sobrecarga
+        public Produto(string nome, double preco)
+        {
+            this.nome = nome;
+            this.preco = preco;
+            this.quantidadeEmEstoque = 0;
+        }
+
+
+
+        public double valorTotalEmEstoque()
+        {
+            return preco * quantidadeEmEstoque;
+        }
+
+        public void realizarEntrada(int quantidade)
+        {
+            quantidadeEmEstoque = quantidadeEmEstoque + quantidade;
+        }
+
+        public void realizarSaida(int quantidade)
+        {
+            quantidadeEmEstoque = quantidadeEmEstoque - quantidade;
+        }
+
+
+        public override string ToString()
+        {
+            return nome
+                + ", R$:  "
+                + preco.ToString("F2", CultureInfo.InstalledUICulture)
+                + ", "
+                + quantidadeEmEstoque
+                + " unidades , Total R$: "
+                + valorTotalEmEstoque().ToString("F2", CultureInfo.InstalledUICulture);
+        }
+    }
+}
---
+++ Exemplo01/Sobrecarga/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+/*
+ E se eu quiser informar no construtor somente o nome e preço do produto, de
+modo que a quantidade inicial de um produto seja 0 (zero) por padrão?
+
+    SOBRECARGA: é o recurso que permite definir mais de uma versão da mesma
+operação, diferenciando-as pela lista de parâmetros.
+
+     */
+
+namespace Sobrecarga
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p;
+
+            Console.Write("Qual o nome do produto:");
+            string nome = Console.ReadLine();
+
+            Console.Write("Qual o preço do produto:");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            p = new Produto(nome, preco);
+
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite a quantidade de produtos que entraram no estoque:");
+            p.realizarEntrada(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite a quantidade de produtos que sairam do estoque:");
+            p.realizarSaida(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.ReadLine();
+        }
+    }
+}
